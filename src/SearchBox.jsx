30dc1d628ef596d0e38c0840868b48f8f662e0a5
Sachin Sisodiya <sachinsisodiya9131185746@gmail.com>
2024-12-20 00:40:05 +0530

    Update SearchBox.jsx
    Change in API key

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }){
     let [city, setCity] = useState("");
     let [error, setError] = useState(false);
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "b3d9461871aebf13f2d82e511f5cbab7";
+    const API_KEY = "key_number";
 
     let getWeatherInfo = async () => {
         try{
